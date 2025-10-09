36fd0ee2f4840b5de451af39603b11e47e721929
chronin7 <liam.perl@ucas-edu.net>
2025-10-09 15:53:03 +0000

    sprites mostly done

--- Platformer/block_maker.py
+++ Platformer/block_maker.py
@@ -1,7 +1,28 @@
+import random
 def print_block():
     for x in range(20):
         for x in range(20):
-            print("🔲",end="")
+            var=random.randint(1,10)
+            #if var<=9:
+            #    print("🟫",end="")
+            #elif var==9:
+            #    print("⬛️",end="")
+            #elif var==8:
+            #    print("⬜️",end="")
+            if var>=3:
+                print("🟥",end="")
+            #elif var==6:
+            #    print("🟪",end="")
+            #elif var==5:
+            #    print("🟦",end="")
+            #elif var==4:
+            #    print("🟩",end="")
+            elif var==2:
+                print("🟨",end="")
+            elif var==1:
+                print("🟧",end="")
+            #elif var==1:
+            #    print("🔲",end="")
         print()
 
-            
+print_block()
