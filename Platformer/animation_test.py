4db37c0cb3f9bfe232a089128504a0c3dff693ac
chronin7 <liam.perl@ucas-edu.net>
2025-10-10 02:30:57 +0000

    working on sprites

---
+++ Platformer/animation_test.py
@@ -0,0 +1,9 @@
+import game_to_png
+import os,time
+while True:
+    os.system('clear')
+    print(game_to_png.sprites_and_tiles.missile_f1)
+    time.sleep(.1)
+    os.system('clear')
+    print(game_to_png.sprites_and_tiles.missile_f2)
+    time.sleep(.1)
